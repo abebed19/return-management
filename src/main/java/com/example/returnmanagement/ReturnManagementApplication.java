d738c790f09b2609234dbb31c7f60fc71d9e9712
Abebe demis <abebedemis@Abebes-MacBook-Pro-3.local>
2026-03-24 15:39:49 +0300

    first commit

---
+++ src/main/java/com/example/returnmanagement/ReturnManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.returnmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReturnManagementApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/returnmanagement/model/Address.java
@@ -0,0 +1,96 @@
+package com.example.returnmanagement.model;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+	
+	private String fullName;
+	private String companyName;
+	private String phone;
+	private String addressLine1;
+	private String addressLine2;
+	private String city;
+	private String state;
+	private String postalCode;
+	private String country;
+	
+	public Address() {
+		
+	}
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	
+	
+
+}
