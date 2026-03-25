9987eba21ac9e32dafc2d4f549d5958f067b153b
abebed19 <abebed19@gmail.com>
2026-03-25 23:19:33 +0300

    Addess and return record DTOs

---
+++ src/main/java/com/example/returnmanagement/dto/AddressDto.java
@@ -0,0 +1,4 @@
+package com.example.returnmanagement.dto;
+
+public record AddressDto(String fullName,String companyName,String phone,String addressLine1,String addressLine2,String city,
+		String state,String postalCode,String country){}
---
+++ src/main/java/com/example/returnmanagement/dto/CreateReturnRecordRequest.java
@@ -0,0 +1,17 @@
+package com.example.returnmanagement.dto;
+
+import com.example.returnmanagement.enums.ResolutionType;
+import com.example.returnmanagement.enums.ReturnRecordStatus;
+import com.example.returnmanagement.model.Address;
+
+public record CreateReturnRecordRequest(String amazonOrderId,
+		String originalZoroOrderId,
+		String returnAuthorizationNumber,
+		String approvedBy,
+		int returnedQuantity,
+		ResolutionType resolutionType,
+		ReturnRecordStatus status,
+		Address fromAddress,
+		Address toAddress
+		
+) {}
