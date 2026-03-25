c67b55f04113f165a41db4537b9a0b0b17ab1dee
abebed19 <abebed19@gmail.com>
2026-03-25 23:40:52 +0300

    controller and service class binding in controller class

--- src/main/java/com/example/returnmanagement/controller/ReturnRecordController.java
+++ src/main/java/com/example/returnmanagement/controller/ReturnRecordController.java
@@ -3,8 +3,20 @@ package com.example.returnmanagement.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.returnmanagement.service.ReturnRecordService;
+
 @RestController
 @RequestMapping("/api/returns")
 public class ReturnRecordController {
+	
+	private ReturnRecordService returnRecordService;
+	
+	public  ReturnRecordController(ReturnRecordService returnRecordService) {
+		this.returnRecordService = returnRecordService;
+	}
+	
+	
+	
+	
 
 }
